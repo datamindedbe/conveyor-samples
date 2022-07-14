0939ddc50a3c75d3f725987d112c225217b9b395
Pascal Knapen <33350052+pascal-knapen@users.noreply.github.com>
2022-07-14 15:55:14 +0000

    making a bit more foolproof

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -e
+
 wget https://app.conveyordata.com/api/info/cli/location/linux/amd64 -O conveyor_linux_amd64.tar.gz
-tar -zxvf conveyor_linux_amd64.tar.gz
-chmod +x bin/linux/amd64/conveyor 
-sudo cp bin/linux/amd64/conveyor /usr/local/bin/conveyor
+tar -zxvf conveyor_linux_amd64.tar.gz --one-top-level
+chmod +x conveyor_linux_amd64/bin/linux/amd64/conveyor 
+sudo cp conveyor_linux_amd64/bin/linux/amd64/conveyor /usr/local/bin/conveyor
 rm conveyor_linux_amd64.tar.gz
+rm -rf conveyor_linux_amd64/
