f7959c8ed0e4b6d7c2e9c1b644eca0fc3c904dbc
Pascal Knapen <33350052+pascal-knapen@users.noreply.github.com>
2022-07-15 14:05:14 +0000

    remote install

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-wget https://app.conveyordata.com/api/info/cli/location/linux/amd64 -O conveyor_linux_amd64.tar.gz
-tar -zxvf conveyor_linux_amd64.tar.gz --one-top-level
-chmod +x conveyor_linux_amd64/bin/linux/amd64/conveyor 
-sudo cp conveyor_linux_amd64/bin/linux/amd64/conveyor /usr/local/bin/conveyor
-rm conveyor_linux_amd64.tar.gz
-rm -rf conveyor_linux_amd64/
