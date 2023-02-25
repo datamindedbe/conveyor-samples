76b109c48ed0fce74b52d6f0e7f726c6d944c983
Niels Claeys <niels.claeys@gmail.com>
2023-02-25 22:08:44 +0100

    Add upstream patch until jwillis releases a newer version.

--- basic/coffee_shop_dbt/dags/coffee_shop_dbt.py
+++ basic/coffee_shop_dbt/dags/coffee_shop_dbt.py
@@ -20,9 +20,27 @@ dag = DAG(
     "coffee_shop_dbt", default_args=default_args, schedule_interval="@daily", max_active_runs=1
 )
 
-ConveyorContainerOperatorV2(
+staging = ConveyorContainerOperatorV2(
     dag=dag,
     task_id="staging",
     aws_role="coffee_shop_dbt-{{ macros.conveyor.env() }}",
-    arguments=["run", "--target", "dev"],
+    arguments=["run", "--target", "dev", "-s", "staging"],
 )
+
+marts = ConveyorContainerOperatorV2(
+    dag=dag,
+    task_id="marts",
+    aws_role="coffee_shop_dbt-{{ macros.conveyor.env() }}",
+    arguments=["run", "--target", "dev", "-s", "marts"],
+)
+
+staging >> marts
+
+dag2 = DAG(
+    "coffee_shop_dbt_factory", default_args=default_args, schedule_interval="@daily", max_active_runs=1
+)
+
+factory = ConveyorDbtTaskFactory(task_aws_role="coffee_shop_dbt-{{ macros.conveyor.env() }}",
+)
+start, end = factory.add_tasks_to_dag(dag=dag2)
+
