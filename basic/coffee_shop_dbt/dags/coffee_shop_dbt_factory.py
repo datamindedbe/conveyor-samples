e725ea280f53948a352a4796589c58c847cc91e3
Stijn De Haes <stijn.de.haes@gmail.com>
2023-02-28 14:43:38 +0100

    Split the dags into 2 (#15)
    Makes explaining what happens a bit more step by step

---
+++ basic/coffee_shop_dbt/dags/coffee_shop_dbt_factory.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from conveyor.factories import ConveyorDbtTaskFactory
+from datetime import timedelta
+from airflow.utils import dates
+
+
+default_args = {
+    "owner": "Conveyor",
+    "depends_on_past": False,
+    "start_date": dates.days_ago(2),
+    "email": [],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "samples_coffee_shop_dbt_factory", default_args=default_args, schedule_interval="@daily", max_active_runs=1,
+)
+
+factory = ConveyorDbtTaskFactory(task_aws_role="conveyor-samples")
+start, end = factory.add_tasks_to_dag(dag=dag)
+
