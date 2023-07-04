d03079440468d36c48bf921c9f8c0378ffd92d51
vlieven <lieven.verswyvel@gmail.com>
2023-07-04 13:38:16 +0200

    Restructure project

---
+++ various/quality_coffee_dbt/README.md
@@ -0,0 +1,5 @@
+# Quality Coffee
+
+A sample dbt project used to illustrate some best practices for testing your dbt code using Conveyor.
+If you want more background information on different kinds of tests you can use with dbt,
+you can refer to blogpost [Testing frameworks in dbt](https://medium.com/datamindedbe/testing-frameworks-in-dbt-3fa8933a5807).
--- various/quality_coffee_dbt/dags/quality_coffee.py
+++ various/quality_coffee_dbt/dags/quality_coffee.py
@@ -20,7 +20,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-
 dag = DAG(
     "samples_quality_coffee",
     default_args=default_args,
@@ -28,40 +27,24 @@ dag = DAG(
     max_active_runs=1,
 )
 
-seed = ConveyorContainerOperatorV2(
-    dag=dag,
-    task_id="seed",
-    aws_role="conveyor-samples",
-    env_vars={"POSTGRES_PASS": AWSParameterStoreValue(name="/conveyor-samples/postgres_password")},
-    arguments=[
-        "seed",
-    ],
-)
+def dbt_task(*, task_id: str, arguments: list[str]) -> ConveyorContainerOperatorV2:
+    return ConveyorContainerOperatorV2(
+        dag=dag,
+        task_id=task_id,
+        aws_role="conveyor-samples",
+        env_vars={
+            "POSTGRES_HOST": AWSParameterStoreValue(name="/conveyor-samples/postgres_host"),
+            "POSTGRES_PASSWORD": AWSParameterStoreValue(name="/conveyor-samples/postgres_password"),
+        },
+        arguments=arguments,
+    )
 
-staging = ConveyorContainerOperatorV2(
-    dag=dag,
-    task_id="staging",
-    aws_role="conveyor-samples",
-    env_vars={"POSTGRES_PASS": AWSParameterStoreValue(name="/conveyor-samples/postgres_password")},
-    arguments=[
-        "run",
-        "--select", "staging",
-    ],
-)
+staging = dbt_task(task_id="staging", arguments=["run", "--select", "staging"])
+marts = dbt_task(task_id="marts", arguments=["run", "--select", "marts"])
 
-marts = ConveyorContainerOperatorV2(
-    dag=dag,
-    task_id="marts",
-    aws_role="conveyor-samples",
-    env_vars={"POSTGRES_PASS": AWSParameterStoreValue(name="/conveyor-samples/postgres_password")},
-    arguments=[
-        "run",
-        "--select", "marts",
-    ],
-)
+staging >> marts
 
-seed >> staging >> marts
-
-if in_production:
-    # Tasks that should only be present in production
-    pass
+if not in_production:
+    # Tasks that should not be present in production
+    seed = dbt_task(task_id="seed", arguments=["seed"])
+    seed >> staging
