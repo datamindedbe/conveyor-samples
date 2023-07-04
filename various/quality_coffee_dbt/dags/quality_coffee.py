eae62c8c4d816bb0048a7d76e9a688f681bc5615
Robbert Hofman <robbert.hofman@dataminded.be>
2023-07-04 23:16:24 +0200

    black formatter

--- various/quality_coffee_dbt/dags/quality_coffee.py
+++ various/quality_coffee_dbt/dags/quality_coffee.py
@@ -27,17 +27,23 @@ dag = DAG(
     max_active_runs=1,
 )
 
+
 def dbt_task(*, task_id: str, arguments: list[str]) -> ConveyorContainerOperatorV2:
     return ConveyorContainerOperatorV2(
         dag=dag,
         task_id=task_id,
         aws_role="conveyor-samples",
         env_vars={
-            "POSTGRES_HOST": AWSParameterStoreValue(name="/conveyor-samples/postgres_host"),
-            "POSTGRES_PASSWORD": AWSParameterStoreValue(name="/conveyor-samples/postgres_password"),
+            "POSTGRES_HOST": AWSParameterStoreValue(
+                name="/conveyor-samples/postgres_host"
+            ),
+            "POSTGRES_PASSWORD": AWSParameterStoreValue(
+                name="/conveyor-samples/postgres_password"
+            ),
         },
         arguments=arguments,
     )
+
 
 def soda_task(*, task_id: str) -> ConveyorContainerOperatorV2:
     return ConveyorContainerOperatorV2(
@@ -45,12 +51,17 @@ def soda_task(*, task_id: str) -> ConveyorContainerOperatorV2:
         task_id=task_id,
         aws_role="conveyor-samples",
         env_vars={
-            "POSTGRES_HOST": AWSParameterStoreValue(name="/conveyor-samples/postgres_host"),
-            "POSTGRES_PASSWORD": AWSParameterStoreValue(name="/conveyor-samples/postgres_password"),
+            "POSTGRES_HOST": AWSParameterStoreValue(
+                name="/conveyor-samples/postgres_host"
+            ),
+            "POSTGRES_PASSWORD": AWSParameterStoreValue(
+                name="/conveyor-samples/postgres_password"
+            ),
         },
         cmds=["bash"],
         arguments=["soda/run_soda.sh"],
     )
+
 
 staging = dbt_task(task_id="staging", arguments=["run", "--select", "staging"])
 marts = dbt_task(task_id="marts", arguments=["run", "--select", "marts"])
