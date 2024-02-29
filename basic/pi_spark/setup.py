b11213f51efb2da83f79dd7cb6f9495e0e32c421
Stijn De Haes <stijndehaes@gmail.com>
2024-02-29 13:23:05 +0100

    Upgrade to spark 3.5.1

--- basic/pi_spark/setup.py
+++ basic/pi_spark/setup.py
@@ -10,7 +10,7 @@ setup(
     name="pi_spark",
     version="0.0.1",
     description="pyspark sample template",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
