0d060f994fecf3c4367c6b9e9878295659b57525
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2025-04-23 11:54:19 +0200

    add connectorx example

--- basic/oracle_to_s3tables_dlthub/src/dlt_example/app.py
+++ basic/oracle_to_s3tables_dlthub/src/dlt_example/app.py
@@ -1,11 +1,18 @@
-from collections.abc import Collection
+import os
+from collections.abc import Collection, Callable, Iterator
 from argparse import ArgumentParser
 
+import pyarrow as pa
 import sqlalchemy as sa
 import dlt
-from dlt.sources import DltSource
+from connectorx import read_sql
+from dlt.sources.credentials import ConnectionStringCredentials
+from dlt.sources import DltSource, DltResource
 from dlt.sources.sql_database import sql_database
 from typing import Any, Optional
+
+from dlt_example.validation.oracle import is_valid_db_object_name
+
 
 # Incremental load: https://dlthub.com/docs/general-usage/incremental-loading
 def incremental_load_select_tables_from_database(
@@ -63,11 +70,16 @@ def full_load_select_tables_from_database(
 
     """
     pipeline = dlt.pipeline(
-        pipeline_name=pipeline_name, destination=destination, dataset_name=dataset_name
+        pipeline_name=pipeline_name,
+        destination=destination,
+        dataset_name=dataset_name,
     )
     source = data_source.with_resources(*table_names)
 
-    info = pipeline.run(source, write_disposition="replace")
+    info = pipeline.run(
+        source,
+        write_disposition="replace",
+    )
 
     print(info)
 
@@ -138,27 +150,101 @@ def type_adapter(column_type: Any) -> Optional[Any]:
     """
     type_name = str(column_type).upper()
     if "INTERVAL YEAR TO MONTH" in type_name:
-        return sa.VARCHAR(255) 
+        return sa.VARCHAR(255)
     if "CLOB" in type_name or "NCLOB" in type_name:
         return sa.Text()
-    if "BLOB" in type_name or "LOB" in type_name or "BFILE" in type_name: #lob is bfile
+    if (
+        "BLOB" in type_name or "LOB" in type_name or "BFILE" in type_name
+    ):  # lob is bfile
         return sa.LargeBinary()
     if "UROWID" in type_name:
-        return sa.String(4000) # max UROWID length in Oracle
+        return sa.String(4000)  # max UROWID length in Oracle
 
-    return None # Let dlt handle other types
+    return None  # Let dlt handle other types
+
+
+def convert_name_of_relation(s: str) -> str:
+    """Return the name of the relation desired in the target system.
+
+    E.g. you could have a view in a source system called 'orders_vw'
+    and would wish it in the target system to have the name 'orders'.
+    It's in functions like these that you can apply this logic.
+    """
+
+    return s
+
+
+def relation_generator(
+    conn_str: ConnectionStringCredentials = dlt.secrets.value,
+    query: str = dlt.config.value,
+    partition_on: str | None = None,
+) -> Iterator[pa.Table]:
+    allowed_core_count = min(
+        len(os.sched_getaffinity(0)), 1
+    )  # will differ from multiprocessing.cpu_count in constrained environments, like Docker containers.
+    yield read_sql(
+        conn=conn_str.to_native_representation(),
+        query=query,
+        partition_on=partition_on,
+        partition_num=allowed_core_count,
+        return_type="arrow",
+        protocol="binary",
+    )
+
+
+def generic_incremental(
+    schema_name: str,
+    relation_name: str,
+    connection_string: str,
+    incremental_column: str,
+    is_valid_db_object_name: Callable[[str], bool] = is_valid_db_object_name,
+    relation_name_converter: Callable[[str], str] = convert_name_of_relation,
+    relation_generator: Iterator[pa.Table] = relation_generator,
+    partition_on: str | None = None,
+) -> DltResource:
+    """Return a dlt Resource suitable for incremental loading using ConnectorX's fast loading mechanism"""
+    # Table names cannot always be bound using the db's main driver API (e.g. python-oracle can't*), so the developer is tasked with validating the table names.
+    # *: See https://python-oracledb.readthedocs.io/en/latest/user_guide/bind.html#binding-column-and-table-names
+    if not is_valid_db_object_name(relation_name):
+        raise ValueError(
+            f"'{relation_name}' is not a valid name for a relation (i.e. table, view)."
+        )
+    if not is_valid_db_object_name(schema_name):
+        raise ValueError(f"'{schema_name}' is not a valid schema name.")
+    query = f"SELECT * FROM {schema_name}.{relation_name}"
+    resource = dlt.resource(
+        name=relation_name_converter(relation_name),
+        write_disposition="merge",
+        standalone=True,
+    )(relation_generator)(connection_string, query, partition_on)
+
+    resource.apply_hints(incremental=dlt.sources.incremental(incremental_column))
+    return resource
 
 
 if __name__ == "__main__":
-
     parser = ArgumentParser()
     parser.add_argument(
         "type",
         default="all",
-        choices=("all", "inc", "full"),
+        choices=("all", "inc", "full", "connx"),
         help="type of loading: incremental, full load or both",
     )
     args = parser.parse_args()
+
+    if args.type in ("connx", "all"):
+        incremental_load = dlt.config["incremental_with_connectorx"]
+        pipe = dlt.pipeline(
+            pipeline_name="foo", destination="duckdb", dataset_name="in_the_output"
+        )
+        source = generic_incremental(
+            schema_name=incremental_load["schema"],
+            relation_name=incremental_load["table_name"],
+            connection_string="",
+            partition_on=incremental_load["partition_column"],
+            incremental_column=incremental_load["incremental_column"],
+        )
+        pipe.run(source, write_disposition="merge")
 
     if args.type in ("inc", "all"):
         incremental_load = dlt.config["incremental_load"]
@@ -176,7 +262,9 @@ if __name__ == "__main__":
 
     if args.type in ("full", "all"):
         full_load = dlt.config["full_load"]
-        full_data_source: DltSource = sql_database(schema=full_load["schema"], type_adapter_callback=type_adapter) 
+        full_data_source: DltSource = sql_database(
+            schema=full_load["schema"], type_adapter_callback=type_adapter
+        )
         full_load_select_tables_from_database(
             data_source=full_data_source,
             pipeline_name=full_load["pipeline_name"],
