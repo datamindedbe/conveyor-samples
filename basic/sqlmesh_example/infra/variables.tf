a51ca9e83407b82695211a97ba25688d1013a0d3
Siyan Luo <luosiyanemily@gmail.com>
2025-04-25 16:07:32 +0200

    add description and default value

--- basic/sqlmesh_example/infra/variables.tf
+++ basic/sqlmesh_example/infra/variables.tf
@@ -1,19 +1,24 @@
 variable "snowflake_username" {
     type = string
+    description = "Please input your DM email address for login the playgroud account"
     sensitive = false
 }
 
 variable "snowflake_password" {
     type = string
+    description = "The password for the service user - sqlmesh_demo_user"
     sensitive = true
 }
 
 variable "source_database" {
     type = string
+    description = "The source database, this is required to grant access to the database"
     sensitive = false
+    default = "SNOWFLAKE_SAMPLE_DATA"
 }
 
 variable "warehouse" {
     type = string
     sensitive = false
+    default = "COMPUTE_WH"
 }
