4bca8d257776c0515d0400e1087743aae10e57ed
jesus garcia ramirez <jesus.garciaramirez@dataminded.com>
2025-04-29 15:09:33 +0200

    Updated infra

--- basic/sqlmesh_example/infra/versions.tf
+++ basic/sqlmesh_example/infra/versions.tf
@@ -11,6 +11,6 @@ terraform {
     bucket       = "terraform-states-dm" # manually created (the bootstrap problem), with versioning enabled (best practice)
     key          = "sqlmesh-demo"
     region       = "eu-west-1"
-    use_lockfile = true
+    # use_lockfile = true
   }
 }
